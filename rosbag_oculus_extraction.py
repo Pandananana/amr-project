624ec06cecfa2b3591fcf98a88bdbd64ca40830e
Pandananana <andreaswendelboe.dk@gmail.com>
2025-04-09 16:29:37 +0200

    Removed unnecessary code

--- rosbag_oculus_extraction.py
+++ rosbag_oculus_extraction.py
@@ -4,6 +4,7 @@ from rosbags.typesys import get_types_from_msg
 from pathlib import Path
 from bps_oculus.core import unpack_data_entry, polar_to_cart
 import cv2
+from cfar import detect_peaks
 
 
 ### SETUP CUSTOM MESSAGE DEFINITION FOR RawData
@@ -31,18 +32,6 @@ add_types.update(get_types_from_msg(msg_text, 'apl_msgs/msg/RawData'))
 
 # Path to your ROS bag (set it correctly)
 bag_path = Path('blue3_2025-03-19-13-58-48.bag')
-# Define the output path (use color video in example)
-output_path = bag_path.with_suffix('.mp4').with_name("oculus_" + bag_path.name)
-
-# # Set up ffmpeg subprocess to write H.265 into MP4
-# ffmpeg = subprocess.Popen([
-#     'ffmpeg',
-#     '-f', 'hevc',             # input is raw H.265 bitstream
-#     '-framerate', '30',       # specify framerate (we know that all camera streams run at 30 fps)
-#     '-i', 'pipe:0',           # read from stdin
-#     '-c', 'copy',             # copy codec (no re-encode)
-#     'output.mp4'              # mux to mp4
-# ], stdin=subprocess.PIPE)
 
 # Create a typestore for the matching ROS release.
 typestore = get_typestore(Stores.ROS1_NOETIC)
@@ -61,30 +50,17 @@ with Reader(bag_path) as reader:
         if timestamp < START_TIMESTAMP:
             continue
 
+
         if connection.topic == '/oculus/raw_data':
             msg = typestore.deserialize_ros1(rawdata, connection.msgtype)
-            ping_result, polar_image_data, new_buffer = unpack_data_entry(msg.data.tobytes()) 
+            _, polar_image_data, _ = unpack_data_entry(msg.data.tobytes()) 
 
             # Skip if no data
-            if ping_result is None:
+            if polar_image_data is None:
                 continue
 
-            cart_image_data = polar_to_cart(polar_image_data)
-            """
-            EXPLAINER
-            The polar_image_data and cart_image_data objects are the extracted backscatter data in polar and
-            cartesian forms respectively.
-            
-            polar_image_data.polar_image: Backscatter image in polar form.
-            polar_image_data.bearing_table: azimuth angle of each pixel in polar_image in radians
-            polar_image_data.ranging_table: range for each pixel in polar_image in m
-            polar_image_data.gain_table: gain applied to each range row (basically time varied gain)
-            
-            cart_image_data.cart_image: Backscatter image in cartesian form.
-            cart_image_data.x_table: X coordinate for each pixel in cart_image in m
-            cart_image_data.y_table: Y coordinate for each pixel in cart_image in m
-            """
-            cv2.imshow("backscatter", cart_image_data.cart_image)
+            cart_image_data = polar_to_cart(polar_image_data).cart_image
+            cv2.imshow("backscatter", cart_image_data)
 
             key = cv2.waitKey(10)
             if key & 0xFF == ord('q'):
